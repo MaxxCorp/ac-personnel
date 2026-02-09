7ca4004ef2524d3e96180ffff79e8333f77040b8
MaxxCorp <bofruchtel@hotmail.com>
2026-02-09 19:39:34 +0100

    initial commit

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,25 @@
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { sveltekitCookies } from 'better-auth/svelte-kit';
+import { env } from '$env/dynamic/private';
+import { getRequestEvent } from '$app/server';
+import { db } from '$lib/server/db';
+
+export const auth = betterAuth({
+	baseURL: env.ORIGIN,
+	secret: env.BETTER_AUTH_SECRET,
+	database: drizzleAdapter(db, { provider: 'pg' }),
+	emailAndPassword: { enabled: true },
+	socialProviders: {
+		google: {
+			clientId: env.GOOGLE_CLIENT_ID!,
+			clientSecret: env.GOOGLE_CLIENT_SECRET!
+		},
+		microsoft: {
+			clientId: env.MICROSOFT_CLIENT_ID!,
+			clientSecret: env.MICROSOFT_CLIENT_SECRET!,
+			tenantId: env.MICROSOFT_TENANT_ID!
+		}
+	},
+	plugins: [sveltekitCookies(getRequestEvent)] // make sure this is the last plugin in the array
+});
